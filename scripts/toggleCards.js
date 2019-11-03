8e09c7e74ca70bab7436437a019558df7839cda2
TheBrellis <brianroyalellis@gmail.com>
2019-11-03 13:18:19 -0500

    toggleCards dropdown now removes and adds cards to the page

--- scripts/toggleCards.js
+++ scripts/toggleCards.js
@@ -7,13 +7,46 @@ let cards = document.querySelectorAll("button[data-action= 'toggle-card']");
 
 $(cards).on("click",function(event){
     event.preventDefault();
-    if ($(this).data("status") === "on"){
-        $(this).addClass("text-muted bg-light");
-        $(this).data("status","off");
-    } else {
-        $(this).removeClass("text-muted bg-light");
-        $(this).data("status","on");
+
+
+if ($(this).data("status") === "on"){
+//Turning card off
+    $(this).addClass("text-muted bg-light");
+    $(this).data("status","off");
+//Toggling card on page
+    if ($(this).data("card") === "word"){
+        $('#wordCard').addClass('d-none');
     }
+
+    if ($(this).data("card") === "weather"){
+        $('#weatherCard').addClass('d-none');
+    }
+
+    if ($(this).data("card") === "joke"){
+        $('#jokeCard').addClass('d-none');
+    }
+
+
+} else {
+// Turning card on
+    $(this).removeClass("text-muted bg-light");
+    $(this).data("status","on");
+// Toggling card on page
+    if ($(this).data("card") === "word"){
+        $('#wordCard').removeClass('d-none');
+    }
+
+    if ($(this).data("card") === "weather"){
+        $('#weatherCard').removeClass('d-none');
+    }
+
+    if ($(this).data("card") === "joke"){
+        $('#jokeCard').removeClass('d-none');
+    }
+
+}  
+
+
 })
 }
 toggleCards();
