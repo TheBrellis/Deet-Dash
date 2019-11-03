d05d7be84904745e03ad90c01d6866573a3d311f
TheBrellis <brianroyalellis@gmail.com>
2019-11-03 13:02:16 -0500

    dropdown elements now toggle between 'muted' and 'unmuted' status depeneding on their data-status

--- scripts/toggleCards.js
+++ scripts/toggleCards.js
@@ -2,11 +2,18 @@ $(document).ready(function() {
 
 
 function toggleCards() {
+// identify elements in drop down
 let cards = document.querySelectorAll("button[data-action= 'toggle-card']");
 
-$(cards).on("click",function(e){
-    e.preventDefault()
-    console.log("hello");
+$(cards).on("click",function(event){
+    event.preventDefault();
+    if ($(this).data("status") === "on"){
+        $(this).addClass("text-muted bg-light");
+        $(this).data("status","off");
+    } else {
+        $(this).removeClass("text-muted bg-light");
+        $(this).data("status","on");
+    }
 })
 }
 toggleCards();
