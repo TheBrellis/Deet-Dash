2cf1cb6c43bbd13eb23b365208c8fa954352482b
TheBrellis <brianroyalellis@gmail.com>
2019-11-06 18:33:33 -0500

    manageCards.js now updates the userInfo object with the correct data-status when the save changes button is clicked

--- scripts/manageCards.js
+++ scripts/manageCards.js
@@ -1,11 +1,27 @@
 $(document).ready(function() {
 
-
-function toggleCards() {
 // identify elements in drop down
 let cards = document.querySelectorAll("button[data-action= 'toggle-card']");
+console.log(userInfo,"before");
+function setCards() {
+
+    for (i=0; i<cardOptions.length;i++){
+    let currentState = $('#manage-buttons').children().eq(i).data("status");
+    userInfo.cards[i].status = currentState;
+    }
+    
+    /*
+    if ($(this).data("card") === "word"){
+        $('#wordCard').addClass('d-none');
+    }
+    */
+   console.log(userInfo,"after");
+}
+
+
 
 //Click Events for Card elements in Modal
+// click event for the selections changes the setting of the selection
 $(cards).on("click",function(event){
     event.preventDefault();
 if ($(this).data("status") === "on"){
@@ -16,21 +32,19 @@ if ($(this).data("status") === "on"){
 // Turning card on
     $(this).removeClass("text-dark bg-light");
     $(this).data("status","on");
-// Updating Elements in Object
 }
-// Loop through the children of $('#manage-buttons')
-// set the index value in the object equal to the data-status of the child. 
-
 })
-}
+
 // Click Event for Save Button in Modal
-function setCards() {
-    if ($(this).data("card") === "word"){
-        $('#wordCard').addClass('d-none');
-    }
-}
+// click event for this updates the object according to the selections, then updates the page according to the object
+$("#submit-cards").on("click",setCards) 
 
-toggleCards();
-setCards();
+
+// click event for the close button set the selections to the current value of the object
+
+
+
+
+
+
 });
-
